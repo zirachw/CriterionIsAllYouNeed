5282662c5273955d1fbe2efd171fcb2499e8537b
Fathur <rizkyfathur326@gmail.com>
2025-11-14 13:52:38 +0700

    init

---
+++ src/allyouneed/linear_model/logistic.py
@@ -0,0 +1,4 @@
+from ..base import BaseClassifier
+
+class LogisticRegression(BaseClassifier):
+    ...
---
+++ src/allyouneed/tree/cart.py
@@ -0,0 +1,4 @@
+from ..base import BaseClassifier
+
+class DecisionTreeClassifier(BaseClassifier):
+    ...
